3845f3ba63425fcbc160ac62b50b74d19240c296
smokyfog <1473092739@qq.com>
2019-09-02 23:31:38 +0800

    项目开始

--- test1/3.接口/index.ts
+++ test1/3.接口/index.ts
@@ -188,11 +188,32 @@ class Clock implements ClockInterface {
 
 
 
+interface ClockConstructor {
+  new (hour: number, minute: number): ClockInterface
+}
+interface ClockInterface {
+  tick()
+}
 
+function createClock(ctor: ClockConstructor, hour: number, minute: number): ClockInterface {
+  return new ctor(hour, minute)
+}
 
+class DigitalClock implements ClockInterface {
+  constructor(h: number, m: number) { }
+  tick() {
+    console.log('beep beep')
+  }
+}
+class AnalogClock implements ClockInterface {
+  constructor(h: number, m: number) { }
+  tick() {
+    console.log('tick tock')
+  }
+}
 
-
-
+let digital = createClock(DigitalClock, 12, 17)
+let analog = createClock(AnalogClock, 7, 32)
 
 
 
